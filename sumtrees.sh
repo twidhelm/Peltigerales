111236ae0d6b109312e6bb9c32843ee7b07a892e
Todd J Widhelm <twidhelm@fieldmuseum.org>
2020-05-16 14:46:19 -0500

    Update sumtrees.sh

--- sumtrees.sh
+++ sumtrees.sh
@@ -6,7 +6,7 @@ for tree1 in *.RAxML_bootstrap.tree
 do
 	tree2=${tree1%%.RAxML_bootstrap.tree}".RAxML_bestTree.tree"
 	tree3=${tree1%%.RAxML_bootstrap.tree}".SumTree.tree"
-	sumtrees.py --min-clade-freq=0.75 --decimals=0 --percentages --target=${tree2} -o ${tree3} --output-tree-format newick ${tree1}
+	sumtrees.py --min-clade-freq=0.33 --decimals=0 --percentages --target=${tree2} -o ${tree3} --output-tree-format newick ${tree1}
 done
 
 # newick format output from SumTrees cannot be read by ASTRAL need to modify file
