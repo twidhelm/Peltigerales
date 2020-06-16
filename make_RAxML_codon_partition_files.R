2056717648f40f132a15227638b12136c2933e36
Todd J Widhelm <twidhelm@fieldmuseum.org>
2020-06-16 14:06:27 -0500

    Update make_RAxML_codon_partition_files.R

--- make_RAxML_codon_partition_files.R
+++ make_RAxML_codon_partition_files.R
@@ -11,6 +11,8 @@ for (fileName in fileNames) {
 a <- read.fasta(fileName)
 b <- summary(a[[1]])$length
 b <- as.numeric(b)
+c <- b-2
+d <- b-1
 
 #newfileName <- writeLines(c(fileName, ".txt"), sep = "")
 
@@ -19,7 +21,7 @@ b <- as.numeric(b)
 # x <- file(newfileName)
 
 sink(fileName)
-cat(c("DNA, p1=1-",b,"\\3,2-",b,"\\3"), sep = "")
+cat(c("DNA, p1=1-",c,"\\3,2-",d,"\\3"), sep = "")
 cat("\n")
 cat(c("DNA, p2=3-",b,"\\3"), sep = "")
 sink()
